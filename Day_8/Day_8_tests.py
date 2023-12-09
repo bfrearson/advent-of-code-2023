8459f2942e5a8f981aeb1c161b4d6d35f8489a7d
Ben Frearson <120649330+benfrearson@users.noreply.github.com>
2023-12-09 07:45:43 +0000

    Day 8 pt2 in progress

--- Day_8/Day_8_tests.py
+++ Day_8/Day_8_tests.py
@@ -19,12 +19,26 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
 """
+test_network2 = """\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"""
 
 class Day_8_tests(unittest.TestCase):
     def test_follow_instructions(self):
         self.assertEqual(2, follow_instructions(test_network1))
         self.assertEqual(6, follow_instructions(test_network2))
 
+    def test_follow_ghost_instructions(self):
+        self.assertEqual(6, follow_ghost_instructions(test_network3))
 
 
 if __name__ == '__main__':
--- Day_8/day_8.py
+++ Day_8/day_8.py
@@ -18,6 +18,32 @@ def follow_instructions(instructions):
 
     return steps
 
+def follow_ghost_instructions(instructions):
+    lines = instructions.splitlines()
+    instruction = lines[0]
+    nodes = [line.split(" = ") for line in lines[2:]]
+    nodes = {node[0]: node[1][1:-1].split(", ") for node in nodes}
+    current_keys = []
+    for key in nodes.keys()
+        if key[2] == "A":
+            current_keys.append(key)
+    steps = 0
+    at_end = False
+    while at_end == False:
+        for char in instruction:
+            # Logic not finished here
+            for index, current_key in enumerate(current_keys):
+                if char == "L":
+                    next_key = nodes[current_key][0]
+                else:
+                    next_key = nodes[current_key][1]
+                current_keys[index] = next_key
+            steps += 1
+            if next_key == "ZZZ":
+                return steps
+
+    return steps
+
 if __name__ == "__main__":
     with open("input.txt") as f:
         instructions = f.read()
