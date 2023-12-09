4906417dd607b8a5b84c42c0c3ffb8c4cfdd4e37
Ben Frearson <120649330+benfrearson@users.noreply.github.com>
2023-12-09 10:49:47 +0000

    Day 9 pt1

--- Day_9/Day_9_tests.py
+++ Day_9/Day_9_tests.py
@@ -1,10 +1,20 @@
 import unittest
-
-
+from day_9 import *
+test_input = """\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+"""
 class Day_9_tests(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
+    def test_compute_next_value(self):
+        expected_values = [18, 28, 68]
+        computed_values = []
+        for line in test_input.splitlines():
+            next_value = compute_next_value([int(number) for number in line.split()])
+            print(next_value)
+            computed_values.append(next_value)
+        self.assertEqual(expected_values, computed_values)
+        self.assertEqual(sum(expected_values), sum(computed_values))
 
 if __name__ == '__main__':
     unittest.main()
--- Day_9/day_9.py
+++ Day_9/day_9.py
@@ -1,8 +1,25 @@
 # Use recursion to find a common difference of 0. Walk back up, providing the next value
 
-def compute_next_value(input: [int]):
+def compute_next_value(sequence: [int]) -> int:
     def find_differences(input: [int]):
+        differences = []
         for index, value in enumerate(input[:-1]):
-            differences = []
             differences.append(input[index + 1] - value)
-            return differences
+        return differences
+
+    differences = find_differences(sequence)
+
+    if all(diff == 0 for diff in differences):
+        return sequence[0]
+    else:
+        difference = compute_next_value(differences)
+        return sequence[-1] + difference
+
+if __name__ == "__main__":
+    file = open("input.txt").read()
+    sequences = [[int(number) for number in line.split()]for line in file.splitlines()]
+    sum = 0
+    for sequence in sequences:
+        next_value = compute_next_value(sequence)
+        sum += next_value
+    print(sum)
