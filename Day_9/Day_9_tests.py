92df8f2778533d40a10686f151387286cb3229bb
Ben Frearson <120649330+benfrearson@users.noreply.github.com>
2023-12-09 10:55:36 +0000

    Day 9 pt2

--- Day_9/Day_9_tests.py
+++ Day_9/Day_9_tests.py
@@ -11,7 +11,15 @@ class Day_9_tests(unittest.TestCase):
         computed_values = []
         for line in test_input.splitlines():
             next_value = compute_next_value([int(number) for number in line.split()])
-            print(next_value)
+            computed_values.append(next_value)
+        self.assertEqual(expected_values, computed_values)
+        self.assertEqual(sum(expected_values), sum(computed_values))
+
+    def test_compute_previous_value(self):
+        expected_values = [-3, 0, 5]
+        computed_values = []
+        for line in test_input.splitlines():
+            next_value = compute_previous_value([int(number) for number in line.split()])
             computed_values.append(next_value)
         self.assertEqual(expected_values, computed_values)
         self.assertEqual(sum(expected_values), sum(computed_values))
